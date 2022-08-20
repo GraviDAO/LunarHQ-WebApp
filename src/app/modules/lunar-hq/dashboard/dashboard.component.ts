0d592ea39f81a701dd7400261b9be0fe2041253e
srinivas bhandage <srini@whyable.com>
2022-08-20 23:51:50 +0530

    dashboard UI

--- src/app/modules/lunar-hq/dashboard/dashboard.component.ts
+++ src/app/modules/lunar-hq/dashboard/dashboard.component.ts
@@ -11,6 +11,43 @@ import {ActivatedRoute, Router} from '@angular/router';
 })
 
 export class DashboardComponent implements OnInit {
+  userDataObj: Array<any> = [
+    {
+      value: [21],
+      label: 'MY SERVERS'
+    },
+    {
+      value: [21, 4],
+      label: 'LICENSES APPLIED VS HELD'
+    },
+    {
+      value: [30],
+      label: 'DISCORD RULES'
+    },
+    {
+      value: [14],
+      label: 'POLLS'
+    },
+    {
+      value: [3],
+      label: 'NEW ANNOUNCEMENTS'
+    }
+  ];
+  myServerCount = 3;
+  serverArrayObj = [1, 2, 3];
+  rulesArrayObj = [
+    {ruleName:'Shepards', population:8,chainName:'Algorand',icon:''},
+    {ruleName:'Watchers on the wall', population:15,chainName:'Chainlink',icon:''},
+    {ruleName:'DAO Fellowship', population:7,chainName:'Polygon',icon:''},
+    {ruleName:'X-Men 2.0', population:9,chainName:'Solana',icon:''},
+    {ruleName:'The Musketeers', population:5,chainName:'Polygon',icon:''},
+    {ruleName:'Avengers', population:22,chainName:'Algorand',icon:''},
+    {ruleName:'Lord of rings', population:22,chainName:'Chainlink',icon:''},
+    {ruleName:'Shepards', population:8,chainName:'Algorand',icon:''},
+    {ruleName:'Watchers on the wall', population:15,chainName:'Chainlink',icon:''},
+    {ruleName:'DAO Fellowship', population:7,chainName:'Polygon',icon:''},
+    {ruleName:'X-Men 2.0', population:9,chainName:'Solana',icon:''},
+  ];
 
 
   constructor(public cssClass: CssConstants,
